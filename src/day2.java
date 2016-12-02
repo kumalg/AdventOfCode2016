237b5401b3504b637907c5e8f1ac35872081792b
kumalg <kumalgfilms@gmail.com>
2016-12-02 20:24:39 +0100

    Day 2 - moar improvments

--- src/day2.java
+++ src/day2.java
@@ -2,15 +2,30 @@ import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Created by Kamil on 02.12.2016.
  */
 public class day2 {
 
+    private ArrayList<String> pin1to9Template = new ArrayList<>(Arrays.asList(new String[]{
+            "123",
+            "456",
+            "789"
+    }));
+
+    private ArrayList<String> pin1toDTemplate = new ArrayList<>(Arrays.asList(new String[]{
+            "  1  ",
+            " 234 ",
+            "56789",
+            " ABC ",
+            "  D  "
+    }));
+
     private ArrayList<String> numsString = new ArrayList<>();
-    private Number1to9 num1to9 = new Number1to9('5');
-    private Number1toD num1toD = new Number1toD('5');
+    private Number num1to9 = new Number(pin1to9Template, '5');
+    private Number num1toD = new Number(pin1toDTemplate, '5');
     private ArrayList<Character> pin1to9 = new ArrayList<>();
     private ArrayList<Character> pin1toD = new ArrayList<>();
 
@@ -31,8 +46,8 @@ public class day2 {
             }
             pin1to9.add(i,num1to9.getActualChar());
             pin1toD.add(i,num1toD.getActualChar());
-            num1to9 = new Number1to9(pin1to9.get(i));
-            num1toD = new Number1toD(pin1toD.get(i));
+            num1to9 = new Number(pin1to9Template, pin1to9.get(i));
+            num1toD = new Number(pin1toDTemplate, pin1toD.get(i));
         }
 
         for (char char1to9 : pin1to9){
@@ -49,337 +64,69 @@ public class day2 {
     public static void main(String[] args) throws IOException{
         new day2();
     }
-
 }
 
-abstract class Number {
-    protected char actualNum;
+class Number {
+
+    private ArrayList<String> pinTemplate = new ArrayList<>();
+    private int xPos = 0, yPos = 0;
 
     public char getActualChar(){
-        return actualNum;
-    }
-}
-
-class Number1toD extends Number{
-
-    Number1toD(char startNum){
-        actualNum = startNum;
-    }
-
-    public void move(char where){
-        switch (actualNum){
-            case '1':{
-                switch (where){
-                    case 'D':
-                        actualNum = '3';
-                        break;
-                }
-                break;
-            }
-
-            case '2':{
-                switch (where){
-                    case 'D':
-                        actualNum = '6';
-                        break;
-                    case 'R':
-                        actualNum = '3';
-                        break;
-                }
-                break;
-            }
-
-            case '3':{
-                switch (where){
-                    case 'U':
-                        actualNum = '1';
-                        break;
-                    case 'D':
-                        actualNum = '7';
-                        break;
-                    case 'L':
-                        actualNum = '2';
-                        break;
-                    case 'R':
-                        actualNum = '4';
-                        break;
-                }
-                break;
-            }
-
-            case '4':{
-                switch (where){
-                    case 'D':
-                        actualNum = '8';
-                        break;
-                    case 'L':
-                        actualNum = '3';
-                        break;
-                }
-                break;
-            }
-
-            case '5':{
-                switch (where){
-                    case 'R':
-                        actualNum = '6';
-                        break;
-                }
-                break;
-            }
-
-            case '6':{
-                switch (where){
-                    case 'U':
-                        actualNum = '2';
-                        break;
-                    case 'D':
-                        actualNum = 'A';
-                        break;
-                    case 'L':
-                        actualNum = '5';
-                        break;
-                    case 'R':
-                        actualNum = '7';
-                        break;
-                }
-                break;
-            }
-
-            case '7':{
-                switch (where){
-                    case 'U':
-                        actualNum = '3';
-                        break;
-                    case 'D':
-                        actualNum = 'B';
-                        break;
-                    case 'L':
-                        actualNum = '6';
-                        break;
-                    case 'R':
-                        actualNum = '8';
-                        break;
-                }
-                break;
-            }
-
-            case '8':{
-                switch (where){
-                    case 'U':
-                        actualNum = '4';
-                        break;
-                    case 'D':
-                        actualNum = 'C';
-                        break;
-                    case 'L':
-                        actualNum = '7';
-                        break;
-                    case 'R':
-                        actualNum = '9';
-                        break;
-                }
-                break;
-            }
-
-            case '9':{
-                switch (where){
-                    case 'L':
-                        actualNum = '8';
-                        break;
-                }
-                break;
-            }
-
-            case 'A':{
-                switch (where){
-                    case 'U':
-                        actualNum = '6';
-                        break;
-                    case 'R':
-                        actualNum = 'B';
-                        break;
-                }
-                break;
-            }
-
-            case 'B':{
-                switch (where){
-                    case 'U':
-                        actualNum = '7';
-                        break;
-                    case 'D':
-                        actualNum = 'D';
-                        break;
-                    case 'L':
-                        actualNum = 'A';
-                        break;
-                    case 'R':
-                        actualNum = 'C';
-                        break;
-                }
-                break;
-            }
-
-            case 'C':{
-                switch (where){
-                    case 'U':
-                        actualNum = '8';
-                        break;
-                    case 'L':
-                        actualNum = 'B';
-                        break;
-                }
-                break;
-            }
-
-            case 'D':{
-                switch (where){
-                    case 'U':
-                        actualNum = 'B';
-                        break;
-                }
-                break;
-            }
-
+        try {
+            return pinTemplate.get(yPos).toCharArray()[xPos];
+        } catch (IndexOutOfBoundsException e){
+            return 'X';
         }
     }
-}
 
-class Number1to9 extends Number{
+    Number(ArrayList<String> pinTemplate, char startChar){
 
-    Number1to9(char startNum){
-        actualNum = startNum;
+        for(int i = 0 ; i < pinTemplate.size() ; i++){
+            if (pinTemplate.get(i).contains(Character.toString(startChar))){
+                yPos = i;
+                for (int j = 0; j < pinTemplate.get(i).length() ; j++){
+                    if (pinTemplate.get(i).toCharArray()[j] == startChar) {
+                        xPos = j;
+                        break;
+                    }
+                }
+
+            }
+        }
+
+        this.pinTemplate = pinTemplate;
     }
 
-    public void move(char where){
-        switch (actualNum){
-            case '1':{
-                switch (where){
-                    case 'D':
-                        actualNum = '4';
-                        break;
-                    case 'R':
-                        actualNum = '2';
-                        break;
-                }
+    public void move(char direction){
+        int newXPos = xPos, newYPos = yPos;
+        char newChar = ' ';
+
+        switch (direction){
+            case 'U' : {
+                newYPos--;
                 break;
             }
-
-            case '2':{
-                switch (where){
-                    case 'D':
-                        actualNum = '5';
-                        break;
-                    case 'L':
-                        actualNum = '1';
-                        break;
-                    case 'R':
-                        actualNum = '3';
-                        break;
-                }
+            case 'D' : {
+                newYPos++;
                 break;
             }
-
-            case '3':{
-                switch (where){
-                    case 'D':
-                        actualNum = '6';
-                        break;
-                    case 'L':
-                        actualNum = '2';
-                        break;
-                }
+            case 'L' : {
+                newXPos--;
                 break;
             }
-
-            case '4':{
-                switch (where){
-                    case 'U':
-                        actualNum = '1';
-                        break;
-                    case 'D':
-                        actualNum = '7';
-                        break;
-                    case 'R':
-                        actualNum = '5';
-                        break;
-                }
+            case 'R' : {
+                newXPos++;
                 break;
             }
+        }
 
-            case '5':{
-                switch (where){
-                    case 'U':
-                        actualNum = '2';
-                        break;
-                    case 'D':
-                        actualNum = '8';
-                        break;
-                    case 'L':
-                        actualNum = '4';
-                        break;
-                    case 'R':
-                        actualNum = '6';
-                        break;
-                }
-                break;
-            }
+        try {
+             newChar = pinTemplate.get(newYPos).toCharArray()[newXPos];
+        } catch (IndexOutOfBoundsException e){}
 
-            case '6':{
-                switch (where){
-                    case 'U':
-                        actualNum = '3';
-                        break;
-                    case 'D':
-                        actualNum = '9';
-                        break;
-                    case 'L':
-                        actualNum = '5';
-                        break;
-                }
-                break;
-            }
-
-            case '7':{
-                switch (where){
-                    case 'U':
-                        actualNum = '4';
-                        break;
-                    case 'R':
-                        actualNum = '8';
-                        break;
-                }
-                break;
-            }
-
-            case '8':{
-                switch (where){
-                    case 'U':
-                        actualNum = '5';
-                        break;
-                    case 'L':
-                        actualNum = '7';
-                        break;
-                    case 'R':
-                        actualNum = '9';
-                        break;
-                }
-                break;
-            }
-
-            case '9':{
-                switch (where){
-                    case 'U':
-                        actualNum = '6';
-                        break;
-                    case 'L':
-                        actualNum = '8';
-                        break;
-                }
-                break;
-            }
-
+        if (newChar != ' '){
+            xPos = newXPos;
+            yPos = newYPos;
         }
     }
 }
