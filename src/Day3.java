4b607e8266b245e2dc841d6376017ab0d7bf9ac0
kumalg <kumalgfilms@gmail.com>
2016-12-03 13:25:52 +0100

    Day 3 - improvments

--- src/Day3.java
+++ src/Day3.java
@@ -1,7 +1,8 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -12,49 +13,51 @@ public class Day3 {
 
     private Day3() throws IOException{
         List<String> listOfTriangles = Files.lines(Paths.get("AdventOfCode/description/day3/input.txt")).collect(Collectors.toList());
-        System.out.println("   By row: " + howManyPossibleTrianglesByRow(listOfTriangles));
-        System.out.println("By column: " + howManyPossibleTrianglesByColumn(listOfTriangles));
+        System.out.println("   By row: " + trianglesByRow(toMatrix(listOfTriangles)));
+        System.out.println("By column: " + trianglesByCol(toMatrix(listOfTriangles)));
     }
 
     public static void main (String[] args) throws IOException{
         new Day3();
     }
 
-    private int howManyPossibleTrianglesByRow(List<String> input){
+    private List<List<Integer>> toMatrix(List<String> list){
+        List<List<Integer>> newList = new ArrayList<>();
+
+        for(String s : list){
+            String[] sidesStrings = s.trim().split(" +");
+            List<Integer> sidesInt = new ArrayList<>();
+
+            for (int i = 0; i < 3; i++)
+                sidesInt.add(Integer.parseInt(sidesStrings[i]));
+
+            newList.add(sidesInt);
+        }
+
+        return newList;
+    }
+
+    private int trianglesByRow( List<List<Integer>> input){
         int numberOfPossibleTriangles = 0;
 
-        for (String s : input){
-            String[] sidesStrings = s.trim().split(" +");
-            int[] sidesInt = new int[3];
-
-            for (int i = 0; i < sidesStrings.length && i < 3; i++)
-                sidesInt[i] = Integer.parseInt(sidesStrings[i]);
-
-            if(new Triangle().isPossible(sidesInt))
+        for (List<Integer> sides : input){
+            if (new Triangle().isPossible(sides))
                 numberOfPossibleTriangles++;
         }
 
         return numberOfPossibleTriangles;
     }
 
-    private int howManyPossibleTrianglesByColumn(List<String> input){
+    private int trianglesByCol( List<List<Integer>> input){
         int numberOfPossibleTriangles = 0;
 
-        for (int row = 0; row < input.size(); row+=3){
-            String[][] sidesOf3Triangles = new String[3][3];
-            int[] sidesInt = new int[3];
-
-            for(int rowInside = 0; rowInside < 3; rowInside++){
-                for(int col = 0; col < 3; col++){
-                    sidesOf3Triangles[rowInside][col] = input.get(rowInside+row).trim().split(" +")[col];
+        for (int row = 0; row < input.size(); row += 3){
+            for (int col = 0; col < 3; col++){
+                List<Integer> sides = new ArrayList<>();
+                for (int rowInside = 0; rowInside < 3; rowInside++){
+                    sides.add(input.get(row+rowInside).get(col));
                 }
-            }
-
-            for (int colmn = 0; colmn < 3; colmn++){
-                for (int rown = 0; rown < 3; rown++)
-                    sidesInt[rown] = Integer.parseInt(sidesOf3Triangles[rown][colmn]);
-
-                if(new Triangle().isPossible(sidesInt))
+                if (new Triangle().isPossible(sides))
                     numberOfPossibleTriangles++;
             }
         }
@@ -64,8 +67,10 @@ public class Day3 {
 }
 
 class Triangle {
-    public boolean isPossible(int[] sidesTable){
-        Arrays.sort(sidesTable);
-        return (sidesTable[0] + sidesTable[1]) > sidesTable[2];
+
+    public boolean isPossible(List<Integer> sidesList){
+
+        Collections.sort(sidesList);
+        return (sidesList.get(0) + sidesList.get(1)) > sidesList.get(2);
     }
 }
