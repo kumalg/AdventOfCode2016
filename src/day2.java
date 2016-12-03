72d511dcfe6b3eb1edf31f77c86237a42f231576
kumalg <kumalgfilms@gmail.com>
2016-12-03 02:14:00 +0100

    Day 2 - final touches

--- src/day2.java
+++ src/day2.java
@@ -1,63 +1,53 @@
-import java.io.BufferedReader;
-import java.io.FileReader;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Created by Kamil on 02.12.2016.
  */
 public class day2 {
 
-    private ArrayList<String> pin1to9Template = new ArrayList<>(Arrays.asList(
+    private ArrayList<String> keypad1to9Template = new ArrayList<>(Arrays.asList(
             "123",
             "456",
             "789"
     ));
 
-    private ArrayList<String> pin1toDTemplate = new ArrayList<>(Arrays.asList(
+    private List<String> keypad1toDTemplate = Arrays.asList(
             "  1  ",
             " 234 ",
             "56789",
             " ABC ",
             "  D  "
-    ));
+    );
 
-    private ArrayList<String> numsString = new ArrayList<>();
-    private Number num1to9 = new Number(pin1to9Template, '5');
-    private Number num1toD = new Number(pin1toDTemplate, '5');
-    private ArrayList<Character> pin1to9 = new ArrayList<>();
-    private ArrayList<Character> pin1toD = new ArrayList<>();
+    private List<String> numsString = new ArrayList<>();
+
+    private String findPin (List<String> keypadTemplate, List<String> input, char startChar) throws Exception{
+        String pin = "";
+        Number number = new Number(keypadTemplate, startChar);
+
+        for (int i = 0; i < input.size(); i++){
+            for (char c : input.get(i).toCharArray())
+                number.move(c);
+
+            char actualChar = number.getActualChar();
+            pin += actualChar;
+
+            number = new Number(keypadTemplate, actualChar);
+        }
+
+        return pin;
+    }
 
     private day2() throws Exception{
+        numsString = Files.lines(Paths.get("AdventOfCode/describtion/day2/input.txt")).collect(Collectors.toList());
 
-        BufferedReader in = new BufferedReader(new FileReader("AdventOfCode/describtion/day2/input.txt"));
-
-        String line;
-        while((line = in.readLine()) != null) {
-            numsString.add(line);
-        }
-        in.close();
-
-        for (int i = 0; i < numsString.size(); i++){
-            for (char c : numsString.get(i).toCharArray()){
-                num1to9.move(c);
-                num1toD.move(c);
-            }
-            pin1to9.add(i,num1to9.getActualChar());
-            pin1toD.add(i,num1toD.getActualChar());
-            num1to9 = new Number(pin1to9Template, pin1to9.get(i));
-            num1toD = new Number(pin1toDTemplate, pin1toD.get(i));
-        }
-
-        for (char char1to9 : pin1to9){
-            System.out.print(char1to9);
-        }
-
-        System.out.print("\n");
-
-        for (char char1toD : pin1toD){
-            System.out.print(char1toD);
-        }
+        System.out.println(findPin(keypad1to9Template, numsString, '5'));
+        System.out.println(findPin(keypad1toDTemplate, numsString, '5'));
     }
 
     public static void main(String[] args) throws Exception{
@@ -67,32 +57,31 @@ public class day2 {
 
 class Number {
 
-    private ArrayList<String> pinTemplate = new ArrayList<>();
+    private List<String> keypadTemplate;
     private int xPos = 0, yPos = 0;
 
     public char getActualChar() throws Exception{
         try {
-            return pinTemplate.get(yPos).charAt(xPos);
+            return keypadTemplate.get(yPos).charAt(xPos);
         } catch (IndexOutOfBoundsException e){
             throw new Exception("Kłopociki :<");
         }
     }
 
-    Number(ArrayList<String> pinTemplate, char startChar){
-
-        for(int i = 0 ; i < pinTemplate.size() ; i++){
+    Number (List<String> pinTemplate, char startChar) {
+        for(int i = 0; i < pinTemplate.size(); i++){
             if (pinTemplate.get(i).contains(Character.toString(startChar))){
-                yPos = i;
-                for (int j = 0; j < pinTemplate.get(i).length() ; j++){
+                for (int j = 0; j < pinTemplate.get(i).length(); j++){
                     if (pinTemplate.get(i).charAt(j) == startChar) {
                         xPos = j;
+                        yPos = i;
                         break;
                     }
                 }
             }
         }
 
-        this.pinTemplate = pinTemplate;
+        this.keypadTemplate = pinTemplate;
     }
 
     public void move(char direction){
@@ -118,9 +107,9 @@ class Number {
             }
         }
 
-        if (newYPos>= 0 && newYPos < pinTemplate.size()){
-            if (newXPos >= 0 && newXPos < pinTemplate.get(newYPos).length()){
-                newChar = pinTemplate.get(newYPos).charAt(newXPos);
+        if (newYPos>= 0 && newYPos < keypadTemplate.size()){
+            if (newXPos >= 0 && newXPos < keypadTemplate.get(newYPos).length()){
+                newChar = keypadTemplate.get(newYPos).charAt(newXPos);
             }
         }
 
